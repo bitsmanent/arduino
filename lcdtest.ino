c41e4850dbfbcad5ded8db344807af162f47ab2c
clamiax <smoppy@gmail.com>
2014-03-15 00:55:47 +0100

    Clean things up and set default values.

--- lcdtest.ino
+++ lcdtest.ino
@@ -3,15 +3,13 @@
 #define LCD_PIN_CONTRAST	6
 #define LCD_PIN_BACKLIGHT 	9
 
-#define CONTRAST_MAX		255
-#define CONTRAST_INC		5
-#define BACKLIGHT_MAX		28836
-#define BACKLIGHT_INC		25
+#define	VALUE_MAX		255
+#define	VALUE_INC		5
 
 #define PROMPT			"> "
 
-static int contrast = 0;
-static int backlight = 0;
+static int contrast = 110;
+static int backlight = 90;
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -19,41 +17,52 @@ void
 handleserial(void) {
 	switch(Serial.read()) {
 		case 'C':
-			if((contrast += CONTRAST_INC) > CONTRAST_MAX)
-				contrast = CONTRAST_MAX;
+			if((contrast += VALUE_INC) > VALUE_MAX)
+				contrast = VALUE_MAX;
 			analogWrite(LCD_PIN_CONTRAST, contrast);
 			Serial.print("Contrast: ");
 			Serial.println(contrast);
 			break;
 		case 'c':
-			if((contrast -= CONTRAST_INC) < 0)
+			if((contrast -= VALUE_INC) < 0)
 				contrast = 0;
 			analogWrite(LCD_PIN_CONTRAST, contrast);
 			Serial.print("Contrast: ");
 			Serial.println(contrast);
 			break;
 		case 'B':
-			if((backlight += BACKLIGHT_INC) > BACKLIGHT_MAX)
-				backlight = BACKLIGHT_MAX;
-			analogWrite(LCD_PIN_BACKLIGHT, contrast);
+			if((backlight += VALUE_INC) > VALUE_MAX)
+				backlight = VALUE_MAX;
+			analogWrite(LCD_PIN_BACKLIGHT, backlight);
 			Serial.print("Backlight: ");
 			Serial.println(backlight);
 			break;
 		case 'b':
-			if((backlight -= BACKLIGHT_INC) < 0)
+			if((backlight -= VALUE_INC) < 0)
 				backlight = 0;
-			analogWrite(LCD_PIN_BACKLIGHT, contrast);
+			analogWrite(LCD_PIN_BACKLIGHT, backlight);
 			Serial.print("Backlight: ");
 			Serial.println(backlight);
 			break;
 		case 'R':
 			Serial.println("Writing \"HELLO WORLD!\" to the LCD display.");
+			lcd.clear();
 			lcd.print("HELLO WORLD!");
 			break;
 		case 'r':
 			Serial.println("Writing \"hello world!\" to the LCD display.");
+			lcd.clear();
 			lcd.print("hello world!");
 			break;
+		case 'L':
+			lcd.blink();
+			Serial.println("Blink ON");
+			break;
+		case 'l':
+			lcd.noBlink();
+			Serial.println("Blink OFF");
+			break;
+
 		default:
 			Serial.println("Unknown command");
 			break;
@@ -66,7 +75,9 @@ void
 setup(void) {
 	Serial.begin(9600);
 	lcd.begin(16, 2); /* 16x2 display */
-	lcd.blink();
+	lcd.write("Hello LCD! ;-)");
+	analogWrite(LCD_PIN_CONTRAST, contrast);
+	analogWrite(LCD_PIN_BACKLIGHT, backlight);
 	Serial.print(PROMPT);
 }
 
@@ -74,6 +85,4 @@ void
 loop(void) {
 	if(Serial.available() > 0)
 		handleserial();
-	lcd.clear();
-	lcd.print("Hello World!");
 }
